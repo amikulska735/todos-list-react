6a2780b75e625dcd55bc75da672e085954ca0c54
Agata Mikulska <amikulska735@gmail.com>
2023-07-03 11:35:45 +0200

    changed index.js

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,8 +1,9 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-  tasks.length > 0 && (
     <div className="buttons__button">
+      {tasks.length > 0 && (
+        <>
       <button className="buttons__button button--markAllDoneButton">
         {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
       </button>
@@ -12,8 +13,9 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
       >
         Ukończ wszystkie
       </button>
+      </>
+      )}
     </div>
-  )
-);
+  );
 
 export default Buttons;
