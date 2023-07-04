b29243e33d5d3b3e8a887b0d7aef5fa39c3cd7f0
Agata Mikulska <amikulska735@gmail.com>
2023-07-04 10:12:22 +0200

    added toggleHideDone and setAllDone to Buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone }) => (
-    <div className="buttons__button">
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
+    <div className="buttons">
       {tasks.length > 0 && (
        <>
-        <button className="buttons__button">
+        <button onClick={toggleHideDone}
+           className="buttons__button">
           {hideDone ? "Pokaż" : "Ukryj"} ukończone
         </button>
         <button
+          onClick={setAllDone}
           className="buttons__button"
           disabled={tasks.every(({ done }) => done)}
         >
