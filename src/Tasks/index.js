035c920c9bcce069fec7313d9565e00f28d1f67d
Agata Mikulska <amikulska735@gmail.com>
2023-07-04 10:13:54 +0200

    added toggleTaskDone and removeTask to Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,15 +1,19 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li 
-            className={`tasks__item${
-                task.done && hideDone 
-                 ? "tasks__item--hidden"
-                  : ""}`} key={task.id}
+            key={task.id}
+            className={`tasks__item${task.done && hideDone 
+                 ? " tasks__item--hidden "
+                  : ""}`
+                } 
             >
-                <button className="tasks__button tasks__button--toggleDone">
+                <button 
+                  className="tasks__button tasks__button--toggleDone"
+                  onClick={() => toggleTaskDone(task.id)}
+                >
                 {task.done ? "✔" : ""}
             </button>
             <span className={
@@ -17,7 +21,10 @@ const Tasks = ({ tasks, hideDone }) => (
                 }>
                     {task.content}
             </span>
-           <button className="tasks__button tasks__button--remove">
+           <button 
+             className="tasks__button tasks__button--remove"
+             onClick={() => removeTask(task.id)}
+             >
              🗑
            </button>
             </li>
