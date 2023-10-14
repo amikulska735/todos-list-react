f194b4d7a964255f8376c750f8f97d4e53cb337e
Agata Mikulska <amikulska735@gmail.com>
2023-10-14 17:23:34 +0200

    added local Storage

--- src/App.js
+++ src/App.js
@@ -1,84 +1,49 @@
-import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Section from "./Section";
 import Buttons from "./Buttons";
 import Header from "./Header";
 import Container from "./Container";
+import useTasks from "./useTasks";
 
 function App() {
-  const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState ([
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść kolację", done: true },
-  ]);
-
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
-
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-
-      return task;
-    }));
-  }
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task, done: true
-    })));
-  };
-
-  const addNewTask = (newTaskContent) => {
-    if (newTaskContent === "") {
-      return;
-    }
-
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content: newTaskContent,
-        done: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ])
-  };
+  const {
+    tasks,
+    hideDone,
+    toggleHideDone,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
 
   return (
-  <Container>
+    <Container>
       <Header title="Lista zadań" />
-      <Section 
-         title ="Dodaj nowe zadanie" 
-         body={<Form addNewTask={addNewTask}/>} 
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
-        body= {
-      <Tasks 
-        tasks={tasks} 
-        hideDone={hideDone} 
-        removeTask={removeTask}
-        toggleTaskDone={toggleTaskDone}
-      />
-    }
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
         extraHeaderContent={
-      <Buttons 
-        tasks={tasks} 
-        hideDone={hideDone}
-        toggleHideDone={toggleHideDone} 
-        setAllDone={setAllDone} 
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
       />
-    }
-      />
-  </Container>
+    </Container>
   );
 }
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -2,26 +2,27 @@ import React, { useState } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        addNewTask(newTaskContent.trim());
-        setNewTaskContent("");
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent.trim());
+      setNewTaskContent("");
+    }
+  };
 
-    return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <input 
-               value={newTaskContent}
-               className="form__input" 
-               placeholder="Co jest do zrobienia?" 
-               onChange={({ target }) => setNewTaskContent(target.value)}
-               autoFocus 
-            />
-            <button className="form__button">Dodaj zadanie
-            </button>
-        </form>
-    )
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        className="form__input"
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        autoFocus
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
 };
 export default Form;
